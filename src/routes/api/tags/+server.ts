47d8e227f21208567f1de6071aee72b4744fcad4
Babak Farkhoopak <44144724+babakfp@users.noreply.github.com>
2024-05-14 00:12:58 +0330

    Improved tags api

--- src/routes/api/tags/+server.ts
+++ src/routes/api/tags/+server.ts
@@ -3,22 +3,30 @@ import { readJsonFileCwd } from "@/lib/server/fs/readJsonFileCwd.js"
 import type { Tags } from "@/lib/apiDataTypes.js"
 import lunr from "lunr"
 
-const ParamsSchema = v.object({ text: v.string() }, v.never())
+const stringOfCommaSeparatedLowercaseWordsRegex = /^([a-z]+,)*[a-z]+$/
+
 const SearchParamsSchema = v.object(
     {
+        search: v.optional(v.string()),
         groupNames: v.optional(
-            v.transform(v.string(), (input) => input.split(","))
+            v.transform(
+                v.string([v.regex(stringOfCommaSeparatedLowercaseWordsRegex)]),
+                (input) => {
+                    const groupNames = input.split(",")
+                    const Schema = v.array(v.string())
+                    const parsedIds = v.parse(Schema, groupNames)
+                    return parsedIds
+                }
+            )
         ),
     },
     v.never()
 )
 
-export const GET = async ({ params, url }) => {
+export const GET = async ({ url }) => {
     try {
-        const { text } = v.parse(ParamsSchema, params)
-
         const searchParams = Object.fromEntries(Array.from(url.searchParams))
-        const { groupNames } = v.parse(SearchParamsSchema, searchParams)
+        const { search, groupNames } = v.parse(SearchParamsSchema, searchParams)
 
         const tags: Tags = await readJsonFileCwd("/data/tags.json")
 
@@ -33,12 +41,16 @@ export const GET = async ({ params, url }) => {
             }, this)
         })
 
-        const searchResult = idx.search(text)
+        if (search) {
+            const searchResult = idx.search(search)
 
-        searchResult.forEach((result) => {
-            const foundTag = tags.find((tag) => tag.id === Number(result.ref))!
-            results.push(foundTag)
-        })
+            searchResult.forEach((result) => {
+                const foundTag = tags.find(
+                    (tag) => tag.id === Number(result.ref)
+                )!
+                results.push(foundTag)
+            })
+        }
 
         if (groupNames?.length) {
             results = results.filter((tag) =>
