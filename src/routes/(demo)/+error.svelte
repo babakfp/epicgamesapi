2e6a0a9c047bbc9a77678fb38d17eae81d61ebea
Babak Farkhoopak <44144724+babakfp@users.noreply.github.com>
2024-05-14 14:23:28 +0330

    Improved demo error page UI

--- src/routes/(demo)/+error.svelte
+++ src/routes/(demo)/+error.svelte
@@ -2,5 +2,10 @@
     import { page } from "$app/stores"
 </script>
 
-<h1>{$page.status}</h1>
-<p>{$page.error?.message}</p>
+<h1 class="font-bold text-white text-6xl">
+    {$page.status}
+</h1>
+
+<p class="mt-2 text-lg text-gray-400">
+    {$page.error?.message}
+</p>
