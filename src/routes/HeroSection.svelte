8dcff80ad46c5da7758c9a2fc8ac4f57e3e6c388
Babak Farkhoopak <44144724+babakfp@users.noreply.github.com>
2024-05-14 14:24:46 +0330

    Used dark colors for the home page

--- src/routes/HeroSection.svelte
+++ src/routes/HeroSection.svelte
@@ -1,22 +1,22 @@
-<div class="bg-white overflow-hidden">
+<div class="overflow-hidden">
     <div class="relative isolate px-6 lg:px-8 overflow-hidden">
         <div
             class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
             aria-hidden="true"
         >
             <div
-                class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem] invert"
                 style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
             />
         </div>
         <div class="mx-auto py-14 max-w-2xl min-h-screen flex items-center">
             <div class="text-center">
                 <h1
-                    class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"
+                    class="text-4xl font-bold tracking-tight text-white sm:text-6xl"
                 >
                     Epic Games API (Unofficial)
                 </h1>
-                <p class="mt-6 text-lg leading-8 text-gray-600">
+                <p class="mt-6 text-lg leading-8 text-gray-400">
                     A simple and unofficial API for Epic Games. This is a
                     work-in-progress and not all endpoints are implemented yet.
                 </p>
@@ -35,7 +35,7 @@
             aria-hidden="true"
         >
             <div
-                class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+                class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem] invert"
                 style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
             />
         </div>
