a3aca6dd494724cf554e49c5410ccdc26f2e24c2
Babak Farkhoopak <44144724+babakfp@users.noreply.github.com>
2024-05-14 18:46:49 +0330

    Added custom theme ("BeardedTheme Arc-eolstorm") to shiki

--- src/routes/docs/+page.server.ts
+++ src/routes/docs/+page.server.ts
@@ -1,17 +1,21 @@
 import { bundledLanguages, bundledThemes, getHighlighter } from "shiki"
 import { readTextFileCwd } from "@/lib/server/fs/readTextFileCwd"
+import { readJsonFileCwd } from "@/lib/server/fs/readJsonFileCwd"
 
 export const load = async () => {
     const content = await readTextFileCwd("/src/routes/docs/content.md")
+    const beardedThemeArcEolstorm = await readJsonFileCwd(
+        "/src/routes/docs/bearded-theme-arc-eolstorm.json"
+    )
 
     const highlighter = await getHighlighter({
-        themes: Object.keys(bundledThemes),
+        themes: [beardedThemeArcEolstorm],
         langs: Object.keys(bundledLanguages),
     })
 
     const html = highlighter.codeToHtml(content, {
         lang: "md",
-        theme: "vitesse-dark",
+        theme: "BeardedTheme Arc-eolstorm",
     })
 
     return { html }
