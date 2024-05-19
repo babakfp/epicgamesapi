23e1c2ea0b9252f5ce746ae79733411809ae26ef
Babak Farkhoopak <44144724+babakfp@users.noreply.github.com>
2024-05-19 08:51:55 +0330

    Fixed tags fetch getting rate limited

--- src/routes/(demo)/products/[id]/+page.server.ts
+++ src/routes/(demo)/products/[id]/+page.server.ts
@@ -3,20 +3,18 @@ import type Tags from "@/lib/data/tags.json"
 import type Products from "@/lib/data/products.json"
 
 export const load = async ({ fetch, params }) => {
-    const res = await fetch(`/api/products/id/${params.id}`)
+    const productResponse = await fetch(`/api/products/id/${params.id}`)
 
-    if (!res.ok) return error(res.status, await res.text())
+    if (!productResponse.ok)
+        return error(productResponse.status, await productResponse.text())
 
-    const product: (typeof Products)[number] = await res.json()
+    const product: (typeof Products)[number] = await productResponse.json()
 
-    const tags = await Promise.all(
-        product.tags.map(
-            async (id) =>
-                (await fetch(`/api/tags/id/${id}`)).json() as Promise<
-                    (typeof Tags)[number]
-                >
-        )
-    )
+    const tagsResponse = await fetch("/api/tags")
+    if (!tagsResponse.ok)
+        return error(tagsResponse.status, await tagsResponse.text())
+    const tags: typeof Tags = await tagsResponse.json()
+    const productTags = tags.filter((tag) => product.tags.includes(tag.id))
 
-    return { product, tags }
+    return { product, tags: productTags }
 }
