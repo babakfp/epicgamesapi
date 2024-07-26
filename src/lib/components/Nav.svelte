916250178933e3329eb7690248eec48d68fd8978
babakfp <babak.bxf@gmail.com>
2024-07-26 19:31:17 +0330

    Fixed nav bg color

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -2,11 +2,15 @@
     import { page } from "$app/stores"
     import NavItem from "$lib/components/NavItem.svelte"
 
-    $: isDocs = $page.url.pathname.startsWith("/docs")
+    $: isHome = $page.url.pathname === "/"
+    $: isDocs = $page.url.pathname === "/docs"
 </script>
 
 <nav class="fixed top-2 z-50 text-xs {isDocs ? 'right-2' : 'inset-x-center'}">
-    <ul class="flex justify-center rounded-full bg-gray-800 shadow">
+    <ul
+        class="flex justify-center rounded-full shadow"
+        class:bg-gray-800={!isHome}
+    >
         <NavItem href="/" class="pl-4">Home</NavItem>
         <NavItem href="/docs">Documentation</NavItem>
         <NavItem href="/products" class="pr-4">Demo</NavItem>
