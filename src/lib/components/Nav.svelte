b973be8f58dcdb57e3b4fa7492d46f3c727d90b5
babakfp <babak.bxf@gmail.com>
2024-07-26 17:52:41 +0330

    Nav styles

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -4,16 +4,18 @@
 </script>
 
 <nav
-    class="fixed top-2 z-50 rounded-full text-xs shadow
-        {$page.url.pathname === '/docs' ? 'right-2' : 'inset-x-center'}
-        {$page.url.pathname.startsWith('/products')
-        ? 'bg-gray-800'
-        : 'backdrop-blur'}
-    "
+    class="fixed top-2 z-50 text-xs {$page.url.pathname.startsWith('/docs')
+        ? 'right-2'
+        : 'inset-x-center'}"
 >
-    <ul class="flex justify-center">
+    <ul
+        class="flex justify-center rounded-full shadow {$page.url.pathname.startsWith(
+            '/products',
+        ) && 'bg-gray-800'}"
+    >
         <NavItem href="/" class="pl-4">Home</NavItem>
         <NavItem href="/docs">Documentation</NavItem>
         <NavItem href="/products" class="pr-4">Demo</NavItem>
     </ul>
+    <div class="absolute -inset-1 -z-1 rounded-full backdrop-blur"></div>
 </nav>
