c53c303a2b1897f12d9bed856c0fdf139f0964a0
babakfp <babak.bxf@gmail.com>
2024-07-26 14:56:29 +0330

    Changed btn colors

--- src/lib/home/HeroSection.svelte
+++ src/lib/home/HeroSection.svelte
@@ -25,7 +25,7 @@
                 >
                     <a
                         href="/products"
-                        class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        class="rounded-md bg-emerald-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600"
                     >
                         Explore the Demo
                     </a>
